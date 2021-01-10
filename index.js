cef26f942d2ec302c91e325e4ba55df84ae2c2cd
George Kandalaft <george.kandalaft@mail.mcgill.ca>
2021-01-10 02:10:38 -0500

    back to where we started

--- index.js
+++ index.js
@@ -24,9 +24,7 @@ mongoose
 
 app.use(
   cors({
-    origin: "https://sharp-hugle-4d2359.netlify.app/",
-    // "http://localhost:4200"
-
+    origin: ["https://sharp-hugle-4d2359.netlify.app", "http://localhost:4200"],
     credentials: true,
   })
 );
